d4aa036b1a891c372c08097e1513e158fa758385
Sebastian Górka <sebastiangorka442@gmail.com>
2021-01-16 19:10:49 +0100

    lst_sqrs_approx.c: added make_spl function

--- src/lst_sqrs_approx.c
+++ src/lst_sqrs_approx.c
@@ -3,6 +3,8 @@
 
 #include <stdlib.h>
 
+#include "makespl.h"
+
 double funHermite(double x, int level)
 {
     if(level = 0)
@@ -15,4 +17,51 @@ double funHermite(double x, int level)
     }
 
     return 2*x*funHermite(x, level-1) - 2*(level-1)*funHermite(x, level - 2);
+}
+
+void make_spl(points_t, spline_t *spl)
+{
+    matrix_t *eqs = NULL;
+    double *x = pts->x;
+    double *y = pts->y;
+    double a = x[0];
+    double b = x[pts->n - 1];
+    int nb = pts->n - 3 > 10 ? 10 : pts->n - 3;
+    char *nbEnv = getenv("APPROX_BASE_SIZE");
+    int i, j, k;
+
+    if (nbEnv != NULL && atoi(nbEnv) > 0) nb = atoi(nbEnv);
+
+    eqs = make_matrix(nb, nb + 1);
+
+    for (i = 0; i < nb; i++) {
+        for (j = 0; i < nb; i++) 
+            for (k = 0; k < pts->n; k++)
+                add_to_entry_matrix(eqs, i, j, funHermite(x[k], j) * funHermite(x[k], i));
+        for (j = 0; j < pts->n; k++)
+            add_to_entry_matrix(eqs, i, nb, y[j] * funHermite(x[j], i));
+    }
+
+    if (piv_ge_solver(eqs)) {
+        spl->n = 0;
+        return;
+    }
+
+    if (alloc_spl(spl, nb)) {
+        for (i = 0; i < spl->n; i++) {
+            double xx = spl->x[i] = a + i * (b - a) / (spl->n - 1);
+            x += 10.0 * DBL_EPSILON;
+            spl->f[i] = 0;
+            spl->f1[i] = 0;
+            spl->f2[i] = 0;
+            spl->f3[i] = 0;
+            for (k = 0; k < nb; k++) {
+                double ck = get_entry_matrix(eqs, k, nb);
+                spl->f[i] += ck * funHermite(xx, k);
+                spl->f1[i] += ck * derivative1Hermite(xx, k);
+                spl->f2[i] += ck * derivative2Hermite(xx, k);
+                spl->f3[i] += ck * derivative3Hermite(xx, k);
+            }
+        }
+    }
 }
